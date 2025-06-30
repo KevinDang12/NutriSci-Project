4717934ae1cb2f228ce8677b83afbf8a076ef1ee
Kevin Dang <kevin.dang@sheridancollege.ca>
2025-06-30 01:06:43 -0400

    Updated MealBuilder & MealFactory

--- NutriSci-Project-EECS3311-Group-Uniform/src/main/java/com/nutrisci/meal/MealBuilder.java
+++ NutriSci-Project-EECS3311-Group-Uniform/src/main/java/com/nutrisci/meal/MealBuilder.java
@@ -1,13 +1,18 @@
 package com.nutrisci.meal;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.nutrisci.calculator.NutritionalData;
+import com.nutrisci.util.UserSessionManager;
 
 // Builder for constructing Meal objects step by step
 public class MealBuilder {
     Meal mealBeingBuilt;
-    boolean mealTypeSet, dataSet;
+    boolean mealTypeSet, dateSet;
     List<String> buildErrors;
+    UserSessionManager userSessionManager;
     
     // Sets the meal type and initializes the meal
     // helped by AI
@@ -15,6 +20,18 @@ public class MealBuilder {
         LocalDate date = LocalDate.now();
         mealBeingBuilt = MealFactory.createMeal(type, date);
         mealTypeSet = true;
+        return this;
+    }
+
+    public MealBuilder setDate(LocalDate date) {
+        mealBeingBuilt.date = date;
+        return this;
+    }
+
+    // Adds a food item to the meal
+    public MealBuilder addFoodItem(String foodName, double quantity) {
+        // Make call to backend to get food item
+        // mealBeingBuilt.addFoodItem(foodItem, quantity);
         return this;
     }
 
@@ -30,6 +47,42 @@ public class MealBuilder {
         return this;
     }
 
+    public MealBuilder updateFoodQuantity(FoodItem item, double quantity) {
+        for (FoodItem foodItem : mealBeingBuilt.foodItems) {
+            if (foodItem.name.equals(item.name)) {
+                foodItem.adjustForQuantity(quantity);
+                break;
+            }
+        }
+        return this;
+    }
+
+    public MealBuilder calculateNutrition() {
+        // mealBeingBuilt.calculateTotalNutrition();
+        // Calls NutritionalCalculator.calculateMealNutrition(foodItems) and assigns to mealBeingBuilt.nutritionalData
+        return this;
+    }
+
+    public MealBuilder addNotes(String notes) {
+        mealBeingBuilt.notes = notes;
+        return this;
+    }
+    
+    public Meal build() {
+        if (!dateSet) {
+            mealBeingBuilt.date = LocalDate.now();
+        }
+
+        // Check if meal type is set, has at least one food item, and total nutrition is reasonable ( not negative and not too high )
+        // if (mealBeingBuilt.getFoodItemCount() >= 1 && mealBeingBuilt.nutritionalData == null) {
+        //     mealBeingBuilt.calculateTotalNutrition();
+        // }
+
+        // Check for any build errors
+        
+        return mealBeingBuilt;
+    }
+
     // Returns a preview of the meal being built
     // helped by AI
     public Meal buildPreview() {
@@ -39,5 +92,21 @@ public class MealBuilder {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public MealBuilder reset() {
+        mealBeingBuilt = null;
+        mealTypeSet = false;
+        dateSet = false;
+        buildErrors = new ArrayList<>();
+        return this;
+    }
+
+    public List<String> getErrors() {
+        return buildErrors;
+    }
+
+    public NutritionalData getCurrentNutrition() {
+        return mealBeingBuilt.nutritionalData;
     }
 }
--- NutriSci-Project-EECS3311-Group-Uniform/src/main/java/com/nutrisci/meal/MealFactory.java
+++ NutriSci-Project-EECS3311-Group-Uniform/src/main/java/com/nutrisci/meal/MealFactory.java
@@ -1,6 +1,7 @@
 package com.nutrisci.meal;
 
 import java.time.LocalDate;
+import java.util.List;
 
 // Factory for creating and duplicating Meal objects
 public class MealFactory {
@@ -31,19 +32,40 @@ public class MealFactory {
         return null;
     }
 
+    // Creates a meal of the given type for the given date and user
     // public static Meal createMeal(MealType type, LocalDate date, User user) {
         
     // }
 
     
+    // Gets all meals for the given date, a backend call
     // public static List<Meal> getMealsForDate(LocalDate date) {
 
     // }
+
+    // Gets all meals for the given date range, a backend call
+    // public static List<Meal> getMealsForDateRange(LocalDate startDate, LocalDate endDate) {
+    //     return null;
+    // }
+
+    public static boolean canAddMeal(MealType type, LocalDate date) {
+        if (type == MealType.SNACK) {
+            return true;
+        }
+
+        // Make call to backend to check if meal can be added
+        return false;
+    }
 
     // Duplicates a meal for a new date
     public static Meal duplicateMeal(Meal originMeal, LocalDate newDate) throws CloneNotSupportedException {
         Meal newMeal = (Meal) originMeal.clone();
         newMeal.date = newDate;
         return newMeal;
+    }
+
+    public static List<MealType> getAvailableMealTypes(LocalDate date) {
+        // Make call to backend to get available meal types
+        return null;
     }
 }
